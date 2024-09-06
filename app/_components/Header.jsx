4e3a0d5f7f883fbe74153942d11cae524fba40aa
BhomyaSharma <122960589+BhomyaSharma@users.noreply.github.com>
2024-09-06 15:43:24 +0530

    Add files via upload

---
+++ app/_components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+function Header() {
+  return (
+    <div className='p-5 flex justify-between items-center border shadow-md'>
+      <Image src ={'./logo.svg'}
+      alt = 'logo'
+      width={60}
+      height={60}
+      />
+      <Button>Get Started</Button>
+      
+    </div>
+  )
+}
+
+export default Header
